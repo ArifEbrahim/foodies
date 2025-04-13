f7c5dcd31587bf19f5f86960643b426aa01ab995
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-04-13 21:45:49 +0100

    individual meals now displayed

--- app/meals/[slug]/page.tsx
+++ app/meals/[slug]/page.tsx
@@ -1,3 +1,37 @@
-export default function MealDetailsPage() {
-  return <div>MealDetails</div>
+import Image from 'next/image'
+import styles from './page.module.css'
+import { getMeal } from '@/lib/meals'
+
+export default async function MealDetailsPage({
+  params
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+  const meal = await getMeal(slug)
+
+  const formattedInstructions = meal.instructions.replace(/\n/g, '<br />')
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: formattedInstructions }}
+        ></p>
+      </main>
+    </>
+  )
 }
--- lib/meals.ts
+++ lib/meals.ts
@@ -1,4 +1,4 @@
-import { Meals } from '@/types/Meals'
+import { Meal, Meals } from '@/types/Meals'
 import sql from 'better-sqlite3'
 
 const db = sql('meals.db')
@@ -6,4 +6,8 @@ const db = sql('meals.db')
 export async function getMeals() {
   await new Promise(resolve => setTimeout(resolve, 1500))
   return db.prepare('SELECT * FROM meals').all() as Meals
+}
+
+export async function getMeal(slug: string) {
+  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug) as Meal
 }
