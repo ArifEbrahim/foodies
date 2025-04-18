3baa545abb348fad0c3daf0d4853e4fea6f20331
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-04-18 07:30:34 +0100

    using useFormState hook

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+
 import ImagePicker from '@/components/Meals/ImagePicker'
 import MealsFormSubmit from '@/components/Meals/MealsFormSubmit'
 import { shareMeal } from '@/lib/actions'
+import { useFormState } from 'react-dom'
 import styles from './page.module.css'
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: '' })
+
   return (
     <>
       <header className={styles.header}>
@@ -13,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form} action={shareMeal}>
+        <form className={styles.form} action={formAction}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,6 +47,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={styles.actions}>
             <MealsFormSubmit />
           </p>
